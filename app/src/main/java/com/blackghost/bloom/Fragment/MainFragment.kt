b6e850640f1ba555d28701d4aac214b5e6db5175
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-04-23 22:15:51 +0200

    Add recyclerView

--- app/src/main/java/com/blackghost/bloom/Fragment/MainFragment.kt
+++ app/src/main/java/com/blackghost/bloom/Fragment/MainFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.blackghost.bloom.R
+import java.io.File
 
 
 class MainFragment : Fragment() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private var photos: List<File> = emptyList()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        return view
     }
 }
