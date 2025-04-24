24c954854652a2e2f4603d6dbe612b2c31631c21
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-04-24 15:00:20 +0200

    Mod PhotoManager

--- app/src/main/java/com/blackghost/bloom/Manager/PhotoManager.kt
+++ app/src/main/java/com/blackghost/bloom/Manager/PhotoManager.kt
@@ -1,4 +1,18 @@
 package com.blackghost.bloom.Manager
 
-class PhotoManager {
+import android.content.Context
+import android.widget.Toast
+import java.io.File
+
+class PhotoManager(private val context: Context) {
+
+    fun createGPhotosFolderIfNeeded() {
+        val externalMediaPath = context.getExternalFilesDir(null)?.absolutePath
+            ?.replace("/Android/data/", "/Android/media/")
+
+        if (externalMediaPath == null) {
+            return
+        }
+
+    }
 }
